21ad9ef6688e88acffe76326944e581865b6ba3f
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-10-31 12:46:16 +0000

    Steps 1-3 completed

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,53 +3,104 @@
            https://api.github.com/users/<your name>
 */
 
+const data = axios.get('https://api.github.com/users/ELAndrews');
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
    Skip to Step 3.
 */
+console.log(data);
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
 
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
+follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+, manually find some other users' github handles, or use the list found 
+at the bottom of the page. Get at least 5 different Github usernames and add them as
+Individual strings to the friendsArray below.
+
+Using that array, iterate over it, requesting data for each user, creating a new card for each
+user, and adding that card to the DOM.
 */
 
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+Using DOM methods and properties, create a component that will return the following DOM element:
 
 <div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
+<img src={image url of user} />
+<div class="card-info">
+<h3 class="name">{users name}</h3>
+<p class="username">{users user name}</p>
+<p>Location: {users location}</p>
+<p>Profile:  
+<a href={address to users github page}>{address to users github page}</a>
+</p>
+<p>Followers: {users followers count}</p>
+<p>Following: {users following count}</p>
+<p>Bio: {users bio}</p>
+</div>
 </div>
 
 */
 
+function card (obj) {
+ const card = document.createElement('div');
+ const img = document.createElement('img');
+ const cardInfo = document.createElement('div');
+ const name = document.createElement('h3');
+ const username = document.createElement('p');
+ const location = document.createElement('p');
+ const profile = document.createElement('p');
+ const profileLink = document.createElement('a');
+ const followers = document.createElement('p');
+ const following = document.createElement('p');
+ const bio = document.createElement('p');
+
+card.append(img);
+card.append(cardInfo);
+cardInfo.append(name);
+cardInfo.append(username);
+cardInfo.append(location);
+cardInfo.append(profile);
+cardInfo.append(followers);
+cardInfo.append(following);
+cardInfo.append(bio);
+profile.append(profileLink);
+
+card.classList.add('card');
+cardInfo.classList.add('ccard-info');
+name.classList.add('name');
+username.classList.add('username');
+
+name.textContent = obj.name;
+username.textContent = obj.login;
+location.textContent = `Location: `, obj.location;
+profile.textContent = `Profile: `;
+profileLink.textContent = obj.html_url;
+followers.textContent = `Followers: `, obj.followers;
+following.textContent = `Following: `, obj.following;
+
+return card
+}
+
 /* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
+tetondan
+dustinmyers
+justsml
+luishrd
+bigknell
 */
+
+// const card = document.querySelector('.card');
+
+// axios.get('https://api.github.com/users/ELAndrews')
+//   .then(response => {
+//     card.append(response);
+//   })
